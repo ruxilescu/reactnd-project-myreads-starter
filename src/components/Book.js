38898df5328bf06a77dc3754ee7982e631da9415
Ruxandra Vasilescu <ruxilescu@gmail.com>
2018-10-18 11:18:52 -0400

    Initial commit with Syntax error: Unterminated regular expression (93:24)

---
+++ src/components/Book.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../BooksAPI';
+
+
+class Book extends React.Component {
+    
+    
+    
+    
+    
+    
+    
+    
+}
+export default Book; 
---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI'
+
+
+class MainPage extends React.Component {
+    
+    
+    
+    
+    
+}
+export default MainPage;
